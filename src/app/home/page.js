9b2b70ca9c8b9f743e0ad101e5d1334b2d611fbc
Ali Al Raiyan <raiyanalial@gmail.com>
2023-08-15 22:58:19 +0600

    second commit

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Link from "next/Link";
 import styles from "./homePage.module.css";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <h1 className={styles.myText}>Welcome To Raiyan's Blog</h1>
+      {/* <Link href={"/blog"}>Blog</Link> */}
       <Link href={"/blog"}>Blog</Link>
     </div>
   );
